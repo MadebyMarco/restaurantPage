a511e26c5338154b544b45c1ca8e17b174fb6db6
MadebyMarco <marco3xmedina@gmail.com>
2023-04-15 10:20:39 -0700

    Install HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
