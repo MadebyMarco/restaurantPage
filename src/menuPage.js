744b9ce81ce38460dcff51a845fbd257f3c1ac95
MadebyMarco <marco3xmedina@gmail.com>
2023-04-19 16:32:25 -0700

    issue: cant use my exported variable

---
+++ src/menuPage.js
@@ -0,0 +1,21 @@
+const menuPage = (() => {
+
+    const createMain = () => {
+        const mainElement = document.createElement("main");
+        mainElement.classList.add = "menu";
+        document.querySelector("#content").appendChild(mainElement);
+    }
+    
+    
+
+    return createMain;
+
+
+
+})();
+
+export {menuPage};
+// Appetizers
+// Main dishes
+// Drinks
+// 
