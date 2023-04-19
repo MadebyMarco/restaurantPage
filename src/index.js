f40b0f171fe3494f83f6792869a407bfdf6d219f
MadebyMarco <marco3xmedina@gmail.com>
2023-04-19 16:31:46 -0700

    add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
+import { createHomePage } from "./homePage";
 import myPage from "./initialPageLoading";
+import { menuPage }   from "./menuPage"
 
 myPage.loadDefault();
 
-function clearContent() {
-    const contents = document.querySelectorAll("#content > *");
-    contents.forEach(element => element.remove());
+
+
+function addEventListenersToNavButtons() {
+
+    const homeButton = document.querySelector(".nav > button");
+
+    homeButton.addEventListener("click", () => {
+        console.log("first button work");
+        myPage.clearContent();
+        createHomePage();
+    });
+
+    const menuButton = document.querySelector(".nav > button:nth-child(2)");
+    
+    menuButton.addEventListener("click", () => {
+        console.log("second button work");
+        myPage.clearContent();
+        menuPage.createMain();
+    });
+
+    const lastButton = document.querySelector(".nav > button:last-child");
+    lastButton.addEventListener("click", () => console.log("last button works too"));
 }
 
-export {clearContent};
+addEventListenersToNavButtons();
