80f05fc7bdf2dc0961ce0b96ab82f3ca23bc6a5b
MadebyMarco <marco3xmedina@gmail.com>
2023-04-20 15:43:57 -0700

    create appetizers

--- src/menuPage.js
+++ src/menuPage.js
@@ -2,13 +2,63 @@ const menuPage = (() => {
 
     const createMain = () => {
         const mainElement = document.createElement("main");
-        mainElement.classList.add = "menu";
+        mainElement.classList.add("menu");
         document.querySelector("#content").appendChild(mainElement);
     }
+
+    const getMain = () => {
+        return document.querySelector(".menu");
+    }
     
+    const createAppetizers = () => {
+        const appetizerContainer = document.createElement("div");
+        const appetizerHeading = document.createElement("h1");
+        appetizerHeading.textContent = "Appetizers";
+        const appetizerUnorderedList = document.createElement("ul");
+        const createAppetizerListItems = () => {
+            for(let i = 0; i < 5; i++) {
+                const item = document.createElement("li");
+
+                switch (i) {
+                    case 0:
+                        item.textContent = "Italian Bread";
+                        break;
+                    case 1:
+                        item.textContent = "Asparagus";
+                        break;
+                    case 2:
+                        item.textContent = "Roasted Eggplant Spread";
+                        break;
+                    case 3:
+                        item.textContent = "Beef Spiedini";
+                        break;
+                    case 4:
+                        item.textContent = "Focaccia Barese";
+                        break;
+                
+                    default: console.log("error with list items");
+                        break;
+                }
+                
+                appetizerUnorderedList.appendChild(item);
+            }
+        } 
+        createAppetizerListItems();
+
+        appetizerContainer.appendChild(appetizerHeading);
+        appetizerContainer.appendChild(appetizerUnorderedList);
+
+        getMain().appendChild(appetizerContainer);
+
+    }
+
+    const load = () => {
+        createMain();
+        createAppetizers();
+    }
     
 
-    return {createMain};
+    return {load};
 
 
 
