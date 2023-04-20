049fd8e5b257cd8083f1d0e8c6a4d88c6a0084fc
MadebyMarco <marco3xmedina@gmail.com>
2023-04-20 16:36:30 -0700

    create ordered list for hours section

--- src/homePage.js
+++ src/homePage.js
@@ -7,12 +7,49 @@ function createHomePage() {
         const about = document.createElement("div");
         about.textContent = "About";
         const aboutDescription = document.createElement("div");
-        aboutDescription.textContent = "Monday: 5am - 12am Tuesday: 5am - 12am Wednesday: 5am - 12am Thursday: 5am - 12am Friday: 5am - 12am Saturday: 5am - 12am Sunday: 5am - 12am";
+        aboutDescription.textContent = "To be decided";
         about.appendChild(aboutDescription);
+        
         
         const hours = document.createElement("div");
         hours.textContent = "Hours";
-        
+        const hoursUnorderedList = document.createElement("ul");
+        const createHoursListItems = () => {
+            for(let i = 0; i < 5; i++) {
+                const item = document.createElement("li");
+
+                switch (i) {
+                    case 0:
+                        item.textContent = "Monday: 5am - 12pm ";
+                        break;
+                    case 1:
+                        item.textContent = "Tuesday: 5am - 12pm" ;
+                        break;
+                    case 2:
+                        item.textContent = "Wednesday: 5am - 12pm";
+                        break;
+                    case 3:
+                        item.textContent = "Thursday: 5am - 12pm ";
+                        break;
+                    case 4:
+                        item.textContent = "Friday: 5am - 12pm";
+                        break;
+                    case 5:
+                        item.textContent = "Saturday: 5am - 12pm";
+                        break;
+                    case 6:
+                        item.textContent = "Sunday: 5am - 12pm";
+                        break;
+                
+                    default: console.log("error with list items");
+                        break;
+                }
+                
+                hoursUnorderedList.appendChild(item);
+            }
+        } 
+        createHoursListItems();
+        hours.appendChild(hoursUnorderedList);
         const location = document.createElement("div");
         location.textContent = "Locations";
         
