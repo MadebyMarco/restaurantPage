403c358861ab03d30e798399ed110e46c336821d
MadebyMarco <marco3xmedina@gmail.com>
2023-04-23 10:44:50 -0700

    create contact information

--- src/contactPage.js
+++ src/contactPage.js
@@ -2,6 +2,48 @@
 console.log("test");
 const contactPage = (() => {
     const createMain = () => {
-        
+        const mainElement = document.createElement("main");
+        return mainElement
     }
+
+    const createContact = () => {
+        const container = document.createElement("div");
+
+        const header = document.createElement("h1");
+        header.textContent = "Contact";
+        container.appendChild(header);
+
+        const numberContact = document.createElement("div");
+        const number = document.createElement("div");
+        const phoneIcon = document.createElement("img");
+        phoneIcon.setAttribute("src", "TBD");
+        number.textContent = "+39339957495";
+
+        numberContact.appendChild(phoneIcon);
+        numberContact.appendChild(number);
+        container.appendChild(numberContact);
+
+        const emailContact = document.createElement("div");
+        const email = document.createElement("div")
+        email.textContent = "fakeItalianEmail@gitaly.com"
+        const emailIcon = document.createElement("img")
+        emailIcon.setAttribute("src", "TBD");
+
+        emailContact.appendChild(emailIcon);
+        emailContact.appendChild(email);
+        container.appendChild(emailContact);
+
+        return container;
+
+    }
+
+    const load = () => {
+        const content = document.querySelector("#content");
+        content.appendChild(createMain());
+        content.appendChild(createContact());
+    }
+
+    return {load};
 })();
+
+export {contactPage}
